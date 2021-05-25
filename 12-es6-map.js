aa2d8ec53a302675191879af91617e11538a6238
arpat9329 <68973655+arpat9329@users.noreply.github.com>
2021-05-25 19:21:16 -0400

    Map can use functions as keys

---
+++ 12-es6-map.js
@@ -0,0 +1,19 @@
+let map = new Map();
+
+map.set('1','str1');
+map.set(1,'num1');
+map.set(true,'bool1');
+
+console.log(map.size);
+
+console.log(map.get(1));
+
+//Map can use objects as keys
+let students={name:'Mike',age:20};
+let visits = new Map();
+visits.set(students,100);
+
+//Map can use functions as keys
+let fun = function(){console.log('Hello');};
+visits.set(fun,'This is function');
+console.log(visits.get(fun));
