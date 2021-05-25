4c4f1793a304a68ecef867e85de5f644c1f0cdc3
arpat9329 <68973655+arpat9329@users.noreply.github.com>
2021-05-25 15:18:18 -0400

    let

---
+++ 9-es6-varavle.js
@@ -0,0 +1,15 @@
+// var x =1;//global and function scoped
+//
+// if (x===1){
+//   var x =2
+//   console.log(x);
+// }
+//
+// console.log(x);
+
+let x = 1;
+if (x===1) {
+  let x =2
+  console.log(x);
+}
+console.log(x);
