e339d17084adda83950edd1f4bf16825c94927af
arpat9329 <68973655+arpat9329@users.noreply.github.com>
2021-05-25 19:36:44 -0400

    constructor

--- 13-es6-class.js
+++ 13-es6-class.js
@@ -5,3 +5,19 @@ class Car {
 
 const honda = new Car();
 console.log(honda instanceof Car);
+
+//constructor
+class Student{
+
+  constructor(id,name){
+    this.id =id;
+    this.name=name;
+  }
+study(){
+  console.log(`${this.name} is studying`);
+  }
+}
+
+let s1=new Student(1,'Mike');
+console.log(`Student : ${s1.id} ${s1.name}`);
+s1.study();
