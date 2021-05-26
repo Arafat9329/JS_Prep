8b945ba37125bcfc9d021ce3373dade7b9f5eb0d
arpat9329 <68973655+arpat9329@users.noreply.github.com>
2021-05-25 20:25:45 -0400

    synchronously

--- 14-es6-promises.js
+++ 14-es6-promises.js
@@ -7,20 +7,51 @@
 
 // console.log('Hello again');
 
-let promiseToCleanTheRoom = new Promise(
-  function(resolve,reject){
-  let isClean =false;
+// let promiseToCleanTheRoom = new Promise(
+//   function(resolve,reject){
+//   let isClean =false;
+//
+//   if(isClean){
+//     resolve('Room is Clean')//this will be assign to then(try) function parameyter
+//   }else {
+//     reject('Room is still dirty')//this will be assign to catch function parameyter
+//   }
+// }
+// )
+//
+// promiseToCleanTheRoom
+//     .then(function(fromResolve){
+//         console.log('The room is '+fromResolve);
+//       })
+//     .catch(function(fromReject){
+//         console.log('The room is '+fromReject);
+//       })
 
-  if(isClean){
-    resolve('Room is Clean')
-  }else {
-    reject('Room is still dirty')
-  }
+//Now wwe have dependencies
+
+let cleanRoom = function(){
+  return new Promise(function(resolve,reject){
+    resolve('Romm is clean');
+  })
 }
-)
 
-promiseToCleanTheRoom.then(function(fromResolve){
-  console.log('The room is '+fromResolve);
-}).catch(function(fromReject){
-  console.log('The room is '+fromReject);
+let removeGarabage = function(msg){
+  return new Promise(function(resolve,reject){
+    resolve(msg+' Garbage is removed');
+  })
+}
+
+let winIceCream = function(msg){
+  return new Promise (function (resolve,reject){
+    resolve(msg+" You got the iceCream")
+  })
+}
+
+//cleanRoom();//promise object
+cleanRoom().then(function(result){
+  return removeGarabage();
+}).then(function(result){
+  return winIceCream()
+}).then(function(result){
+  console.log('Finished '+result);
 })
